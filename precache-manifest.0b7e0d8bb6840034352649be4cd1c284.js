3e46a0ae7a13dbaf9c75798cdad25328cb78e6d9
sasakuvac <sasakuvac@gmail.com>
2020-09-25 11:20:19 +0200

    Updates

---
+++ precache-manifest.0b7e0d8bb6840034352649be4cd1c284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9354dddfe9be342fdb7ee76e39e3f96",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "328a089c364546034243",
+    "url": "/monsters-rolodex/static/css/main.04ee2d3b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328a089c364546034243",
+    "url": "/monsters-rolodex/static/js/main.c9407480.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
